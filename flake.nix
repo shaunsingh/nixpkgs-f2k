ce1506f1e50944a065d83e584366602da8d949a2
shaunsingh <shaunsingh0207@gmail.com>
2021-11-15 17:26:15 -0500

    add macOS support

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
         });
       };
     }
-    // flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+    // flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
